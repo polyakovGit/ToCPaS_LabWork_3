9d353bbfe71cdf073459b189510a6d61a857929b
BuildTools <unconfigured@null.spigotmc.org>
2021-10-07 05:20:05 +0500

    Add project files.

---
+++ lab3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Program
+    {
+        //ALTER TABLE t1 ADD COLUMN col1 int CHECK((col1 + 2) * 3 < 5))
+        static void Main(string[] args)
+        {
+        }
+    }
+}
